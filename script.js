8f57c6ddd2b2a27ef643a2414211c8f89dce0754
PanupongSoongwiriya <61070345@kmitl.ac.th>
2019-03-12 15:07:14 +0700

    random

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-for( var i = 0 ; i < 100 ; i++ ){
-	var div = document.createElement("<div></div>");
-	body.element.appendChild(div);
+setInterval(function(){
+	x = getRandomInt(0, 11);
+	y = 0;
+	document.querySelector('.enemy1').setAttribute('x',x);
+	document.querySelector('.enemy1').setAttribute('y',y);
+},1000)
+
+setInterval(function(){
+	x = getRandomInt(1, 10);
+	y = 9;
+	document.querySelector('.enemy2').setAttribute('x',x);
+	document.querySelector('.enemy2').setAttribute('y',y);
+},1000)
+
+setInterval(function(){
+	x = 0;
+	y = getRandomInt(0, 11);
+	document.querySelector('.enemy3').setAttribute('x',x);
+	document.querySelector('.enemy3').setAttribute('y',y);
+},1000)
+
+setInterval(function(){
+	x = 9;
+	y = getRandomInt(1, 10);
+	document.querySelector('.enemy4').setAttribute('x',x);
+	document.querySelector('.enemy4').setAttribute('y',y);
+},1000)
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
