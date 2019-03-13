4d43834702613ebe7afbda5b549ac8b961925ad3
Kotchatan Chatchoeidaeng <44992872+ICopKungI@users.noreply.github.com>
2019-03-13 15:05:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,58 @@
+<<<<<<< HEAD
+var x, y, i, j = -1, k = 0, z;
+//random
+=======
 var x, y, i, j = -1, k;
 
+>>>>>>> da149c64473fced7fb2053d6689dbdf2babd7987
 setInterval(function(){
 	x = getRandomInt(0, 9);
 	y = 0;
-	document.querySelector('.enemy1').setAttribute('x',x);
-	document.querySelector('.enemy1').setAttribute('y',y);
+	document.querySelector('.random1').setAttribute('x',x);
+	document.querySelector('.random1').setAttribute('y',y);
 },1000)
 
 setInterval(function(){
 	x = getRandomInt(1, 10);
 	y = 9;
-	document.querySelector('.enemy2').setAttribute('x',x);
-	document.querySelector('.enemy2').setAttribute('y',y);
+	document.querySelector('.random2').setAttribute('x',x);
+	document.querySelector('.random2').setAttribute('y',y);
 },1000)
 
 setInterval(function(){
 	x = 9;
 	y = getRandomInt(0, 9);
-	document.querySelector('.enemy3').setAttribute('x',x);
-	document.querySelector('.enemy3').setAttribute('y',y);
+	j++;
+	if (j == 0) {
+		z = y;
+	}
+	document.querySelector('.random3').setAttribute('x',x);
+	document.querySelector('.random3').setAttribute('y',y);
+	document.querySelector('.test').setAttribute('x',j);
+	document.querySelector('.test').setAttribute('y',z);
 },1000)
 
 setInterval(function(){
 	x = 0;
 	y = getRandomInt(1, 10);
-	document.querySelector('.enemy4').setAttribute('x',x);
-	document.querySelector('.enemy4').setAttribute('y',y);
+	document.querySelector('.random4').setAttribute('x',x);
+	document.querySelector('.random4').setAttribute('y',y);
 },1000)
 
+<<<<<<< HEAD
+//
+
+/*setInterval(function(){
+	j++
+	for (i = 0; i < 3; i++) {
+		if (j > 9) {
+			break;
+		}
+		var name = '.ansbox' + i;
+		document.querySelector(name).setAttribute('x',j);
+	}
+},1000)*/
+=======
 setInterval(function(){
 	x = 4;
 	j++;
@@ -39,6 +64,7 @@ setInterval(function(){
 	document.querySelector('.test').setAttribute('j',j);
 },1000)
 
+>>>>>>> da149c64473fced7fb2053d6689dbdf2babd7987
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
